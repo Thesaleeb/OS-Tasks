348822595fdeea5e0f50e6d6fb04e161ab18f813
Thesaleeb <Thesaleeb@gmail.com>
2026-03-09 18:52:22 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,99 +1,109 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+
 using namespace std;
-
-
-// each frame stores a page and its aging counter
-struct Frame
-{
-    int page;
-    unsigned int age;
-};
-
-
-// simulate paging using Aging algorithm
-int simulate(vector<int> refs , int framesCount)
-{
-    vector<Frame> memory;   // represents RAM
-    int faults = 0;         // counts page faults
-
-    // go through all page references
-    for(int page : refs)
-    {
-        // ---- aging step ----
-        // shift all counters right (older pages lose importance)
-        for(auto &f : memory)
-            f.age >>= 1;
-
-        int hit = -1;
-
-        // check if page already exists in memory
-        for(int i = 0 ; i < memory.size() ; i++)
-        {
-            if(memory[i].page == page)
-                hit = i;
-        }
-
-        // ---- PAGE HIT ----
-        if(hit != -1)
-        {
-            // mark page as recently used (set MSB)
-            memory[hit].age |= 128;
-        }
-        else
-        {
-            // ---- PAGE FAULT ----
-            faults++;
-
-            // if free frame available
-            if(memory.size() < framesCount)
-            {
-                // add new page
-                memory.push_back({page,128});
-            }
-            else
-            {
-                // find page with smallest age (oldest page)
-                int victim = 0;
-
-                for(int i = 1 ; i < memory.size() ; i++)
-                {
-                    if(memory[i].age < memory[victim].age)
-                        victim = i;
-                }
-
-                // replace oldest page
-                memory[victim] = {page,128};
-            }
-        }
-    }
-
-    return faults;
-}
-
-
 
 int main()
 {
-    ifstream file("pages.txt");   // input file
-    vector<int> refs;
-    int x;
+    ifstream f("input.txt");   // open file
 
-    // read page references from file
-    while(file >> x)
-        refs.push_back(x);
+    int p , r;
+    f >> p;   // number of processes
+    f >> r;   // number of resources
 
-    cout<<"Frames\tFaults/1000\n";
+    int E[10];        // existing resources
+    int C[10][10];    // allocation
+    int R[10][10];    // requests
 
-    // test different numbers of frames
-    for(int f = 1 ; f <= 10 ; f++)
+    int i , j;
+
+    // read existing resources
+    for(i=0;i<r;i++)
+        f >> E[i];
+
+    // read allocation matrix
+    for(i=0;i<p;i++)
+        for(j=0;j<r;j++)
+            f >> C[i][j];
+
+    // read request matrix
+    for(i=0;i<p;i++)
+        for(j=0;j<r;j++)
+            f >> R[i][j];
+
+
+    int avail[10];
+
+    // calculate available resources
+    for(i=0;i<r;i++)
     {
-        int faults = simulate(refs , f);
+        int used = 0;
 
-        // normalize faults per 1000 references
-        cout<<f<<"\t"
-            << (double)faults / refs.size() * 1000
-            << endl;
+        for(j=0;j<p;j++)
+            used += C[j][i];
+
+        avail[i] = E[i] - used;
     }
+
+
+    bool done[10];
+
+    // mark all processes unfinished
+    for(i=0;i<p;i++)
+        done[i] = false;
+
+
+    bool change = true;
+
+    while(change)
+    {
+        change = false;
+
+        for(i=0;i<p;i++)
+        {
+            if(done[i] == false)
+            {
+                bool ok = true;
+
+                for(j=0;j<r;j++)
+                {
+                    if(R[i][j] > avail[j])
+                        ok = false;
+                }
+
+                if(ok)
+                {
+                    // release resources
+                    for(j=0;j<r;j++)
+                        avail[j] += C[i][j];
+
+                    done[i] = true;
+                    change = true;
+                }
+            }
+        }
+    }
+
+
+    bool deadlock = false;
+
+    for(i=0;i<p;i++)
+        if(done[i] == false)
+            deadlock = true;
+
+
+    if(deadlock == false)
+        cout<<"No deadlock in the system"<<endl;
+    else
+    {
+        cout<<"Deadlock detected\n";
+        cout<<"Processes: ";
+
+        for(i=0;i<p;i++)
+            if(done[i] == false)
+                cout<<"P"<<i<<" ";
+
+        cout<<endl;
+    }
+
 }
